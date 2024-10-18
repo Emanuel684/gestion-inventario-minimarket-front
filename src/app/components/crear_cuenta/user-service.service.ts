79a98e67826df84ee5b8c5cf7f19cc82e5c93d21
emanuel-ig <emanuel.acevedo@wizeline.com>
2024-10-18 06:53:42 -0500

    fix: correction in some modules

--- src/app/components/crear_cuenta/user-service.service.ts
+++ src/app/components/crear_cuenta/user-service.service.ts
@@ -1,6 +1,6 @@
 // import { Injectable } from '@angular/core'
-// import { HttpClient, HttpHeaders } from '@angular/common/http'
-// import { Observable } from 'rxjs'
+import { HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs'
 
 // @Injectable({
 //   providedIn: 'root',
@@ -30,7 +30,29 @@ import { HttpClient } from '@angular/common/http'
 
 @Injectable({ providedIn: 'root' })
 export class ConfigService {
-  constructor(private http: HttpClient) {
-    // This service can now make HTTP requests via `this.http`.
+  private apiUrl = 'https://api.example.com/users' // Replace with your API endpoint
+
+  // constructor(private http: HttpClient) {}
+  constructor() {
+    console.log('this')
   }
+
+  fetchUsers() {
+    console.log('this')
+    return []
+  }
+
+  // createUser(user: any): Observable<any> {
+  //   const headers = new HttpHeaders({ 'Content-Type': 'application/json' })
+  //   return this.http.post<any>(this.apiUrl, user, { headers })
+  // }
+
+  // deleteUser(userId: number): Observable<any> {
+  //   return this.http.delete<any>(`${this.apiUrl}/${userId}`)
+  // }
 }
+// export class ConfigService {
+//   // constructor(private http: HttpClient) {
+//   //   // This service can now make HTTP requests via `this.http`.
+//   // }
+// }
