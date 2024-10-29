ca59c0fb5a59c51985bbe580def4cb44f45b5143
emanuel684 <emanuelacag@gmail.com>
2024-10-29 17:09:59 -0500

    feat: add inventary interaction

--- src/app/components/inventario/inventario.component.ts
+++ src/app/components/inventario/inventario.component.ts
@@ -1,17 +1,8 @@
-import { CommonModule, formatCurrency } from '@angular/common'
+import { CommonModule } from '@angular/common'
 import { Component, OnInit } from '@angular/core'
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
-// import { ToastrService } from 'ngx-toastr';
-// import { ContactService } from '../../../services/contact.service';
-// import { LoginService } from '../../../services/login.service';
-// import { UserProfileService } from '../../../services/user-profile.service';
+import { ProductosService } from '../../services/productos.services'
 
 @Component({
   selector: 'app-inventario',
@@ -23,40 +14,14 @@ import { RouterModule } from '@angular/router'
 })
 export class InventarioComponent implements OnInit {
   title = 'Inventario'
-  contactForm!: FormGroup
-  userId = 0
-  constructor(
-    private fb: FormBuilder,
-    // private contactService: ContactService,
-    // private toastrService: ToastrService,
-    // private loginService: LoginService,
-    // private userService:UserProfileService
-  ) {
-    this.contactForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      subject: ['', Validators.required],
-      message: ['', Validators.required],
-    })
-  }
+
+  productos: any = {}
+
+  constructor(private productosService: ProductosService) {}
 
   ngOnInit(): void {
     console.log('oninit')
+    this.productos = this.productosService.getAllProductos()
     // this.userId = this.loginService.userId;
-  }
-
-  onSubmit() {
-    if (this.contactForm.valid) {
-      console.log(this.contactForm.value)
-      // this.contactService.sendMessage(this.contactForm.value).subscribe(
-      //   (response) => {
-      //     this.toastrService.success('Message sent successfully!');
-      //     this.contactForm.reset(); // Reset form after submission
-      //   },
-      //   (error) => {
-      //     this.toastrService.error('Error sending message. Please try again.');
-      //   }
-      // );
-    }
   }
 }
